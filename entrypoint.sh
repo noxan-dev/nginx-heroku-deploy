45e4266569f85cba40dbb0cec1acffd5bf392db4
Chaim Malek <chaimmalek@gmail.com>
2023-04-03 13:11:13 -0400

    Init

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH="/opt/venv/bin:$PATH"
+# Set the port to use for Gunicorn
+export RUN_PORT=8000
+
+# Start Gunicorn as a background process
+gunicorn run:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
+
+# Start Nginx in the foreground
+nginx -g 'daemon off;'
