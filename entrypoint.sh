f6a39ce11b058762ec163c859605bbf64ab8ddbb
Chaim Malek <chaimmalek@gmail.com>
2023-04-04 14:00:51 -0400

    Gunicorn and Nginx finally running together

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export PATH="/opt/venv/bin:$PATH"
 export RUN_PORT=8000
 
 # Start Gunicorn as a background process
-gunicorn run:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
+gunicorn run:app --bind 0.0.0.0:5000 --log-level=debug --workers=4
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
